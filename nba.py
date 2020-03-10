0092eef846db28331cffe00b52cb325a68b458d4
Zach Smith <zsmith126028@gmail.com>
2020-03-10 10:56:59 -0500

    add function that checks if the response is good

--- nba.py
+++ nba.py
@@ -19,8 +19,14 @@ def get_url(url):
     log_error('Error during request to {0} : {1}'.format(url, str(e)))
     return None
 
-
-
+def is_good_response(resp):
+  """
+  Returns True if the response seems to be HTML, False otherwise
+  """
+  content_type = resp.headers['Content-Type'].lower()
+  return (resp.status_code == 200
+    and content_type is not None
+    and content_type.find('html') > -1)
 
 def log_error(e):
   """
