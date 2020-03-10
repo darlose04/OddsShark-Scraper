bffe14c338f21ff73baafdf4d02784bc0b49c5a2
Zach Smith <zsmith126028@gmail.com>
2020-03-10 10:52:16 -0500

    add function to get content at url

--- nba.py
+++ nba.py
@@ -2,3 +2,19 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+
+def get_url(url):
+  """
+  Gets the content at url by making a HTTP GET request
+  """
+
+  try:
+    with closing(get(url, stream=True)) as resp:
+      if is_good_response(resp):
+        return resp.content
+      else:
+        return None
+
+  except RequestException as e:
+    log_error('Error during request to {0} : {1}'.format(url, str(e)))
+    return None
