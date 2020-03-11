badea75e6de75f8056014af60282b22d9996291a
Zach Smith <zsmith126028@gmail.com>
2020-03-11 17:08:41 -0500

    add dictionary to push game objects into array

--- nba.py
+++ nba.py
@@ -55,27 +55,22 @@ def get_tables():
       for caption in table.select('caption'):
         away_team = caption.select('.table__name-short')[0].text
         home_team = caption.select('.table__name-short')[1].text
-        print("{0} at {1}".format(away_team, home_team))
+        
       
       for item in table.select('tbody > tr > td'):
         
         if table.select('tbody > tr > td').index(item) == 1:
           predictedScore = item.text.split()
-          # print(predictedScore)
           away_team_score = float(predictedScore[0])
           home_team_score = float(predictedScore[2])
           total_score = away_team_score + home_team_score
-          print("Predicted Away Score: {0}".format(away_team_score))
-          print("Predicted Home Score: {0}".format(home_team_score))
-          print("Predicted Total Score: {0}".format(total_score))
+          
 
         if table.select('tbody > tr > td').index(item) == 4:
           computer_spread_pick = item.text
-          print("Computer Spread Pick: {0}".format(computer_spread_pick))
         
         if table.select('tbody > tr > td').index(item) == 5:
           computer_OU_pick = item.text
-          print("Computer Over Under Pick: {0}".format(computer_OU_pick))
 
         if item.text == "Public Consensus":
 
@@ -83,23 +78,46 @@ def get_tables():
             
             if row.text.split()[0] == away_team or row.text.split()[0] == home_team:
               public_spread_pick = row.text
-              print("Public Spread Pick: {0}".format(public_spread_pick))
 
             if row.text.split()[0] == 'Over' or row.text.split()[0] == 'Under':
               public_OU_pick = row.text
-              print("Public O/U Pick: {0}".format(public_OU_pick))
         
         if table.select('tbody > tr > td').index(item) == 10:
           public_spread_percent = item.text
-          print("Consensus Spread %: {0}".format(public_spread_percent))
+          
 
         if table.select('tbody > tr > td').index(item) == 11:
           public_OU_percent = item.text
-          print("Consensus O/U %: {0}".format(public_OU_percent))
+          
 
+      game_dict = {
+        'away_team': away_team,
+        'home_team': home_team,
+        'predicted_away_score': away_team_score,
+        'predicted_home_score': home_team_score,
+        'total_score': total_score,
+        'comp_spread_pick': computer_spread_pick,
+        'comp_ou_pick': computer_OU_pick,
+        'public_spread_pick': public_spread_pick,
+        'public_ou_pick': public_OU_pick,
+        'public_consensus_spread_%': public_spread_percent,
+        'public_consensus_ou_%': public_OU_percent
+      }
 
-      print("=================")
+      game_info.append(game_dict)
+      # print("=================")
+      # print("{0} at {1}".format(away_team, home_team))
+      # print("Predicted Away Score: {0}".format(away_team_score))
+      # print("Predicted Home Score: {0}".format(home_team_score))
+      # print("Predicted Total Score: {0}".format(total_score))
+      # print("Computer Spread Pick: {0}".format(computer_spread_pick))
+      # print("Computer Over Under Pick: {0}".format(computer_OU_pick))
+      # print("Public Spread Pick: {0}".format(public_spread_pick))
+      # print("Public O/U Pick: {0}".format(public_OU_pick))
+      # print("Consensus Spread %: {0}".format(public_spread_percent))
+      # print("Consensus O/U %: {0}".format(public_OU_percent))
+  print(game_info)
       
-
+  
 
 get_tables()
